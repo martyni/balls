dafe763d8abb82c08aa42d150c98e5c00abe6693
martyni <martyn@automationlogic.com>
2020-11-29 21:19:31 +0000

    alex smells

--- balls_coming_to_you_in_3d.py
+++ balls_coming_to_you_in_3d.py
@@ -4,12 +4,18 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+from OpenGL.GL.ARB.occlusion_query import *
+from OpenGL.GL.HP.occlusion_test import *
+from OpenGL.GLUT import glutSolidCube
+import numpy as np
 import math
+from banner import banner
 
 from random import randint
 
 pygame.init()
-display = (1680, 1050) 
+WIDTH,HEIGHT = 1920, 1080
+display = (WIDTH, HEIGHT) 
 scree = pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
 glEnable(GL_DEPTH_TEST)
@@ -63,17 +69,96 @@ def event_handling(run):
     return paused, run, mouseMove
 
 
-def draw_ball(x, y, z, r, g, b):
-    glTranslatef(x, y, z)
-    glColor4f(r, g, b, 1)
-    gluSphere(sphere, 1.0, 32, 16) 
 
 X,Y,Z = [0,0,0]
-SPEED = 0.1
+SPEED = 1
 
 def cycle(speed=500, offset=0, function=math.sin):
     source = pygame.time.get_ticks()/speed
     return function(source + offset)
+
+
+
+def draw_crosshair_in_2d():
+    glPushMatrix()
+    '''
+    glViewport(0, 0, WIDTH, HEIGHT)'''
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0, WIDTH, HEIGHT, 0, -1, 1)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    glColor3ub(240, 240, 240)
+    glLineWidth(2.0)
+    glBegin(GL_LINES)
+    glVertex2i(int(WIDTH / 2 - 7), int(HEIGHT / 2))
+    glVertex2i(int(WIDTH / 2 + 7), int(HEIGHT / 2))
+    glEnd()
+    glBegin(GL_LINES)
+    glVertex2i(int(WIDTH / 2), int(HEIGHT / 2 + 7))
+    glVertex2i(int(WIDTH / 2), int(HEIGHT / 2 - 7))
+    glEnd()
+    glPopMatrix()
+
+
+def draw_crosshair_3d():
+    crosshair = np.array([
+      -0.02, 0,
+       0.02, 0,
+       0, -0.02,
+       0,  0.02], dtype = 'float32')
+    vbo_2d, vao_2d = glGenBuffers(1), glGenVertexArrays(1)
+    
+    glBindVertexArray(vao_2d)
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_2d)
+    glBufferData(GL_ARRAY_BUFFER, crosshair, GL_STATIC_DRAW)
+    
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 8, ctypes.c_void_p(0))
+    glEnableVertexAttribArray(0)
+    
+        #render 3d stuff with another shader program and other vbos ad vaos
+    
+    glBindVertexArray(0)
+
+    
+    '''shader_program_2d.use()'''
+    glBindVertexArray(vao_2d)
+    glDrawArrays(GL_LINES, 0, 4)
+
+def draw_rect(x, y, width, height):
+    glBegin(GL_QUADS)                                  # start drawing a rectangle
+    glVertex2f(x, y)                                   # bottom left point
+    glVertex2f(x + width, y)                           # bottom right point
+    glVertex2f(x + width, y + height)                  # top right point
+    glVertex2f(x, y + height)                          # top left point
+    glEnd()
+    glLoadIdentity()                                   # reset position
+
+    glColor3f(0.0, 0.0, 1.0)                           # set color to blue
+
+def draw_floor():
+    glPushMatrix()
+    glColor4f(0.5, 0.5, 0.5, 1)
+    glBegin(GL_QUADS)
+    glVertex3f(-10, -10, -2)
+    glVertex3f(10, -10, -2)
+    glVertex3f(10, 10, -2)
+    glVertex3f(-10, 10, -2)
+    glEnd()
+
+def draw_fake_floor():
+    glPushMatrix()
+    glColor4f(0.5, 0.5, 0.5, 1)
+    glBegin(GL_QUADS)
+    glEnd()
+
+def draw_ball(x, y, z, r, g, b, mouseMove, viewMatrix, size=1.0, k=32, l=16):
+    glTranslatef(x, y, z)
+    glColor4f(r, g, b, 1)
+    gluSphere(sphere, size, k, l) 
+    glTranslatef(-x, -y, -z)
+
+
 
 while run:
     paused, run, mouseMove = event_handling(run)
@@ -107,7 +192,18 @@ while run:
             glTranslatef(SPEED,0,0)
             X += SPEED
 
-
+        if keypress[pygame.K_UP]:
+            #glTranslatef(0,-SPEED,0)
+            #Y -= SPEED
+            glRotatef(-SPEED*10, 0.0, 0.0, 1.0)
+        if keypress[pygame.K_DOWN]:
+            #glTranslatef(0,SPEED,0)
+            #Y += SPEED
+            glRotatef(SPEED*10, 0.0, 0.0, 1.0)
+        if keypress[pygame.K_RIGHT]:
+            glRotatef(SPEED*10, 0.0, 1.0, 0.0)
+        if keypress[pygame.K_LEFT]:
+            glRotatef(-SPEED*10, 0.0, 1.0, 0.0)
         # apply the left and right rotation
         glRotatef(mouseMove[0]*SPEED, 0.0, 1.0, 0.0)
 
@@ -122,25 +218,28 @@ while run:
         glLightfv(GL_LIGHT0, GL_POSITION, [1, -1, 1, 0])
 
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        #draw_floor()
+        draw_fake_floor()
 
-        glPushMatrix()
+        #draw_ball(0.5 - X, 1 + Z, Y , 1 , 0 , 0  , mouseMove, viewMatrix ) 
+        #draw_ball(2 - X, 1 + Z, Y , 0 , 0.5 , 0  , mouseMove, viewMatrix ) 
+        #draw_ball(-3.0 - X,  Z, Y , 0 , 0.5 , 0  , mouseMove, viewMatrix ) 
+        #draw_ball(-3 - X,  0, Y , 0 , 1 , 0  , mouseMove, viewMatrix ) 
+        #draw_ball(0,  0, -Z , 0 , 0 , 1  , mouseMove, viewMatrix ) 
+        #draw_ball(0,  0, 0 , 0.5 , 0.5 , 0  , mouseMove, viewMatrix , -Z) 
+        #draw_ball(0,  0, 0 , 0.0 , 0.5 , 0.5  , mouseMove, viewMatrix , -X) 
+        #draw_rect(5 , 3, WIDTH, HEIGHT )
+        lines = [ line for line in banner.split('\n') ]
 
-        glColor4f(0.5, 0.5, 0.5, 1)
-        glBegin(GL_QUADS)
-        glVertex3f(-10, -10, -2)
-        glVertex3f(10, -10, -2)
-        glVertex3f(10, 10, -2)
-        glVertex3f(-10, 10, -2)
-        glEnd()
-        glTranslatef(-1.5, 0, 0)
-        glColor4f(0.5, 0.2, 0.2, 1)
-        gluSphere(sphere, 1.0, 32, 16) 
 
-        glTranslatef(3, 0, 0)
-        glColor4f(0.2, 0.2, 0.5, 1)
-        gluSphere(sphere, 1.0, 32, 16) 
-
-        draw_ball(5 - X, 0 + Z, 0 , 0,0.5,0) 
+        for y in range(0, len(lines)):
+           for x in range(0, len(lines[0])):
+              for z in range(0, 1):
+                  if lines[y][x] !=' ' and lines[y][x] !='_':
+                     #draw_ball(x, z, y, math.cos(x), math.sin(y), math.sin(z), mouseMove, viewMatrix) 
+                     draw_ball(x/2, -z * 2, -y, math.sin(x), math.cos(y), 2, mouseMove, viewMatrix,0.3) 
+        glEnable(GL_CULL_FACE)
+        glMatrixMode(GL_MODELVIEW)
 
         glPopMatrix()
 
---
+++ banner.py
@@ -0,0 +1,9 @@
+banner='''                                                                                                 
+           88                                                                   88 88            
+           88                                                                   88 88            
+           88                                                                   88 88            
+,adPPYYba, 88  ,adPPYba, 8b,     ,d8    ,adPPYba, 88,dPYba,,adPYba,   ,adPPYba, 88 88 ,adPPYba,  
+       `Y8 88 a8P____888  `Y8, ,8P'     I8[       88P'   "88"    "8a a8P____888 88 88 I8[        
+,adPPPPP88 88 8PP""""       )888(        `"Y8ba,  88      88      88 8PP""""    88 88  `"Y8ba,   
+88,    ,88 88 "8b,     a  ,d8" "8b,           ]8I 88      88      88 "8b,     a 88 88       ]8I  
+`"8bbdP"Y8 88  `"Ybbd8"  8P'     `Y8    `"YbbdP"' 88      88      88  `"Ybbd8"' 88 88 `"YbbdP"'  '''
